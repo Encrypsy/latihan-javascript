a09de0f22c9f175c56408d57b7af35186aad0b7e
Malya Maritza <malyamaritza07@gmail.com>
2025-04-15 20:08:09 +0700

    Variabel

---
+++ module-2-variabel/index.js
@@ -0,0 +1,45 @@
+// ini inline-comment
+
+/*
+
+    kalo ini multiple-line-comment
+
+*/
+
+// VAR //
+
+/*
+var = bisa diakses di luar lingkup nya (scope nya)
+*/
+
+if (true){
+    var nama = "Malya";
+    nama = 'Winter'
+} // statement if adalah scope nya si var
+
+console.log("var: ", nama)
+
+// LET //
+
+if (true){
+    let name = "Karina";
+    name = 'Nenengisme'
+    console.log("let: ", name) //tidak error, karena berada di dalam scope nya
+} 
+
+console.log("let: ", name) //error, karena 'let' tidak bisa di akses di luar scope nya
+
+// * VAR DAN LET BISA DIUBAH VALUENYA
+
+// CONST //
+
+/*
+
+    const = tidak bisa diakses di luar scopenya dan valuenya TIDAK BISA diubah
+
+*/
+
+if (true) {
+    const apa = 10;
+    console.log("const: ", apa)
+}
